111706d61add9d6d7eb057fe2e1e06b7db946e4f
Yankeevader <36558243+yankeevader@users.noreply.github.com>
2026-05-21 02:36:36 -0700

    Clean up incomplete railway casing block state handling
    Replaced raw MCreator Property/Comparable casts in IncompleterailwaydecorativecasingBlock with typed FACING access.
    
    This preserves the original placement, rotation, mirror, light-blocking, and block property behavior while removing unchecked warnings.

--- src/main/java/net/mcreator/create_mf/block/IncompleterailwaydecorativecasingBlock.java
+++ src/main/java/net/mcreator/create_mf/block/IncompleterailwaydecorativecasingBlock.java
@@ -14,36 +14,45 @@ import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.properties.DirectionProperty;
 import net.minecraft.world.level.block.state.properties.NoteBlockInstrument;
-import net.minecraft.world.level.block.state.properties.Property;
 
-public class IncompleterailwaydecorativecasingBlock
-extends Block {
+public class IncompleterailwaydecorativecasingBlock extends Block {
     public static final DirectionProperty FACING = HorizontalDirectionalBlock.FACING;
 
     public IncompleterailwaydecorativecasingBlock() {
-        super(BlockBehaviour.Properties.of().ignitedByLava().instrument(NoteBlockInstrument.BASS).sound(SoundType.CHERRY_WOOD).strength(6.0f, 10.0f));
-        this.registerDefaultState((BlockState)((BlockState)this.stateDefinition.any()).setValue((Property)FACING, (Comparable)Direction.NORTH));
+        super(BlockBehaviour.Properties.of()
+                .ignitedByLava()
+                .instrument(NoteBlockInstrument.BASS)
+                .sound(SoundType.CHERRY_WOOD)
+                .strength(6.0f, 10.0f));
+
+        this.registerDefaultState(this.stateDefinition.any()
+                .setValue(FACING, Direction.NORTH));
     }
 
+    @Override
     public int getLightBlock(BlockState state, BlockGetter worldIn, BlockPos pos) {
         return 15;
     }
 
+    @Override
     protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
         super.createBlockStateDefinition(builder);
-        builder.add(new Property[]{FACING});
+        builder.add(FACING);
     }
 
+    @Override
     public BlockState getStateForPlacement(BlockPlaceContext context) {
-        return (BlockState)super.getStateForPlacement(context).setValue((Property)FACING, (Comparable)context.getHorizontalDirection().getOpposite());
+        return super.getStateForPlacement(context)
+                .setValue(FACING, context.getHorizontalDirection().getOpposite());
     }
 
-    public BlockState rotate(BlockState state, Rotation rot) {
-        return (BlockState)state.setValue((Property)FACING, (Comparable)rot.rotate((Direction)state.getValue((Property)FACING)));
+    @Override
+    public BlockState rotate(BlockState state, Rotation rotation) {
+        return state.setValue(FACING, rotation.rotate(state.getValue(FACING)));
     }
 
-    public BlockState mirror(BlockState state, Mirror mirrorIn) {
-        return state.rotate(mirrorIn.getRotation((Direction)state.getValue((Property)FACING)));
+    @Override
+    public BlockState mirror(BlockState state, Mirror mirror) {
+        return rotate(state, mirror.getRotation(state.getValue(FACING)));
     }
 }
-
