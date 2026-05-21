db207df03aaa561ba71ea1dd8c25cd4c21917f92
Yankeevader <36558243+yankeevader@users.noreply.github.com>
2026-05-21 02:34:57 -0700

    Clean up upper Sulfur Lamp block state handling
    Replaced raw MCreator Property/Comparable casts in SulfurlampupBlock with typed FACING and WATERLOGGED access.
    
    This preserves the original directional placement, upper lamp shape, emissive/light behavior, rotation, mirror, skip-render, and waterlogging behavior while removing unchecked warnings.

--- src/main/java/net/mcreator/create_mf/block/SulfurlampupBlock.java
+++ src/main/java/net/mcreator/create_mf/block/SulfurlampupBlock.java
@@ -5,7 +5,6 @@ import net.minecraft.core.Direction;
 import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.LevelAccessor;
-import net.minecraft.world.level.LevelReader;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.DirectionalBlock;
 import net.minecraft.world.level.block.Mirror;
@@ -18,79 +17,101 @@ import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.properties.BlockStateProperties;
 import net.minecraft.world.level.block.state.properties.BooleanProperty;
 import net.minecraft.world.level.block.state.properties.DirectionProperty;
-import net.minecraft.world.level.block.state.properties.Property;
-import net.minecraft.world.level.material.Fluid;
 import net.minecraft.world.level.material.FluidState;
 import net.minecraft.world.level.material.Fluids;
 import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.Shapes;
 import net.minecraft.world.phys.shapes.VoxelShape;
 
-public class SulfurlampupBlock
-extends Block
-implements SimpleWaterloggedBlock {
+public class SulfurlampupBlock extends Block implements SimpleWaterloggedBlock {
     public static final DirectionProperty FACING = DirectionalBlock.FACING;
     public static final BooleanProperty WATERLOGGED = BlockStateProperties.WATERLOGGED;
 
     public SulfurlampupBlock() {
-        super(BlockBehaviour.Properties.of().sound(SoundType.LANTERN).strength(0.05f, 10.0f).lightLevel(s -> 15).noOcclusion().hasPostProcess((bs, br, bp) -> true).emissiveRendering((bs, br, bp) -> true).isRedstoneConductor((bs, br, bp) -> false));
-        this.registerDefaultState((BlockState)((BlockState)((BlockState)this.stateDefinition.any()).setValue((Property)FACING, (Comparable)Direction.NORTH)).setValue((Property)WATERLOGGED, (Comparable)Boolean.valueOf(false)));
+        super(BlockBehaviour.Properties.of()
+                .sound(SoundType.LANTERN)
+                .strength(0.05f, 10.0f)
+                .lightLevel(state -> 15)
+                .noOcclusion()
+                .hasPostProcess((bs, br, bp) -> true)
+                .emissiveRendering((bs, br, bp) -> true)
+                .isRedstoneConductor((bs, br, bp) -> false));
+
+        this.registerDefaultState(this.stateDefinition.any()
+                .setValue(FACING, Direction.NORTH)
+                .setValue(WATERLOGGED, false));
     }
 
+    @Override
     public boolean skipRendering(BlockState state, BlockState adjacentBlockState, Direction side) {
-        return adjacentBlockState.getBlock() == this ? true : super.skipRendering(state, adjacentBlockState, side);
+        return adjacentBlockState.getBlock() == this || super.skipRendering(state, adjacentBlockState, side);
     }
 
+    @Override
     public boolean propagatesSkylightDown(BlockState state, BlockGetter reader, BlockPos pos) {
         return state.getFluidState().isEmpty();
     }
 
+    @Override
     public int getLightBlock(BlockState state, BlockGetter worldIn, BlockPos pos) {
         return 0;
     }
 
+    @Override
     public VoxelShape getVisualShape(BlockState state, BlockGetter world, BlockPos pos, CollisionContext context) {
         return Shapes.empty();
     }
 
+    @Override
     public VoxelShape getShape(BlockState state, BlockGetter world, BlockPos pos, CollisionContext context) {
-        return switch ((Direction)state.getValue((Property)FACING)) {
-            default -> SulfurlampupBlock.box((double)0.0, (double)14.0, (double)0.0, (double)16.0, (double)16.0, (double)1.0);
-            case Direction.NORTH -> SulfurlampupBlock.box((double)0.0, (double)14.0, (double)15.0, (double)16.0, (double)16.0, (double)16.0);
-            case Direction.EAST -> SulfurlampupBlock.box((double)0.0, (double)14.0, (double)0.0, (double)1.0, (double)16.0, (double)16.0);
-            case Direction.WEST -> SulfurlampupBlock.box((double)15.0, (double)14.0, (double)0.0, (double)16.0, (double)16.0, (double)16.0);
-            case Direction.UP -> SulfurlampupBlock.box((double)0.0, (double)0.0, (double)14.0, (double)16.0, (double)1.0, (double)16.0);
-            case Direction.DOWN -> SulfurlampupBlock.box((double)0.0, (double)15.0, (double)0.0, (double)16.0, (double)16.0, (double)2.0);
+        return switch (state.getValue(FACING)) {
+            default -> SulfurlampupBlock.box(0.0D, 14.0D, 0.0D, 16.0D, 16.0D, 1.0D);
+            case NORTH -> SulfurlampupBlock.box(0.0D, 14.0D, 15.0D, 16.0D, 16.0D, 16.0D);
+            case EAST -> SulfurlampupBlock.box(0.0D, 14.0D, 0.0D, 1.0D, 16.0D, 16.0D);
+            case WEST -> SulfurlampupBlock.box(15.0D, 14.0D, 0.0D, 16.0D, 16.0D, 16.0D);
+            case UP -> SulfurlampupBlock.box(0.0D, 0.0D, 14.0D, 16.0D, 1.0D, 16.0D);
+            case DOWN -> SulfurlampupBlock.box(0.0D, 15.0D, 0.0D, 16.0D, 16.0D, 2.0D);
         };
     }
 
+    @Override
     protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
         super.createBlockStateDefinition(builder);
-        builder.add(new Property[]{FACING, WATERLOGGED});
+        builder.add(FACING, WATERLOGGED);
     }
 
+    @Override
     public BlockState getStateForPlacement(BlockPlaceContext context) {
-        boolean flag = context.getLevel().getFluidState(context.getClickedPos()).getType() == Fluids.WATER;
-        return (BlockState)((BlockState)super.getStateForPlacement(context).setValue((Property)FACING, (Comparable)context.getNearestLookingDirection().getOpposite())).setValue((Property)WATERLOGGED, (Comparable)Boolean.valueOf(flag));
+        boolean waterlogged = context.getLevel().getFluidState(context.getClickedPos()).getType() == Fluids.WATER;
+
+        return super.getStateForPlacement(context)
+                .setValue(FACING, context.getNearestLookingDirection().getOpposite())
+                .setValue(WATERLOGGED, waterlogged);
     }
 
-    public BlockState rotate(BlockState state, Rotation rot) {
-        return (BlockState)state.setValue((Property)FACING, (Comparable)rot.rotate((Direction)state.getValue((Property)FACING)));
+    @Override
+    public BlockState rotate(BlockState state, Rotation rotation) {
+        return state.setValue(FACING, rotation.rotate(state.getValue(FACING)));
     }
 
-    public BlockState mirror(BlockState state, Mirror mirrorIn) {
-        return state.rotate(mirrorIn.getRotation((Direction)state.getValue((Property)FACING)));
+    @Override
+    public BlockState mirror(BlockState state, Mirror mirror) {
+        return rotate(state, mirror.getRotation(state.getValue(FACING)));
     }
 
+    @Override
     public FluidState getFluidState(BlockState state) {
-        return (Boolean)state.getValue((Property)WATERLOGGED) != false ? Fluids.WATER.getSource(false) : super.getFluidState(state);
+        return state.getValue(WATERLOGGED)
+                ? Fluids.WATER.getSource(false)
+                : super.getFluidState(state);
     }
 
+    @Override
     public BlockState updateShape(BlockState state, Direction facing, BlockState facingState, LevelAccessor world, BlockPos currentPos, BlockPos facingPos) {
-        if (((Boolean)state.getValue((Property)WATERLOGGED)).booleanValue()) {
-            world.scheduleTick(currentPos, (Fluid)Fluids.WATER, Fluids.WATER.getTickDelay((LevelReader)world));
+        if (state.getValue(WATERLOGGED)) {
+            world.scheduleTick(currentPos, Fluids.WATER, Fluids.WATER.getTickDelay(world));
         }
+
         return super.updateShape(state, facing, facingState, world, currentPos, facingPos);
     }
 }
-
