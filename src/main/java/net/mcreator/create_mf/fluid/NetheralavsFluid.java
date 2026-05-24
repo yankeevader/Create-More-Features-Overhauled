6f6b9962b986e7df6e360d6d09e11f7644b2566c
Yankeevader <36558243+yankeevader@users.noreply.github.com>
2026-05-24 16:59:39 -0700

    Clean Netheralavs fluid generics
    - Removed raw Property casts from Netheralavs fluid state handling
    - Replaced unsafe LEVEL property access with typed LEVEL_FLOWING usage
    - Cleaned fluid state code without changing behavior

--- src/main/java/net/mcreator/create_mf/fluid/NetheralavsFluid.java
+++ src/main/java/net/mcreator/create_mf/fluid/NetheralavsFluid.java
@@ -1,57 +1,52 @@
 package net.mcreator.create_mf.fluid;
 
-import net.mcreator.create_mf.init.CreateMfModBlocks;
 import net.mcreator.create_mf.init.CreateMfModFluidTypes;
 import net.mcreator.create_mf.init.CreateMfModFluids;
-import net.mcreator.create_mf.init.CreateMfModItems;
-import net.minecraft.core.particles.ParticleOptions;
-import net.minecraft.core.particles.ParticleTypes;
-import net.minecraft.world.item.Item;
-import net.minecraft.world.level.block.LiquidBlock;
-import net.minecraft.world.level.block.state.StateDefinition;
-import net.minecraft.world.level.block.state.properties.Property;
+
+import net.neoforged.neoforge.fluids.BaseFlowingFluid;
+
 import net.minecraft.world.level.material.Fluid;
 import net.minecraft.world.level.material.FluidState;
-import net.neoforged.neoforge.fluids.BaseFlowingFluid;
-import net.neoforged.neoforge.fluids.FluidType;
+import net.minecraft.world.level.block.state.properties.BlockStateProperties;
 
-public abstract class NetheralavsFluid
-extends BaseFlowingFluid {
-    public static final BaseFlowingFluid.Properties PROPERTIES = new BaseFlowingFluid.Properties(() -> (FluidType)CreateMfModFluidTypes.NETHERALAVS_TYPE.get(), () -> (Fluid)CreateMfModFluids.NETHERALAVS.get(), () -> (Fluid)CreateMfModFluids.FLOWING_NETHERALAVS.get()).explosionResistance(100.0f).tickRate(7).levelDecreasePerBlock(2).slopeFindDistance(6).bucket(() -> (Item)CreateMfModItems.NETHERALAVS_BUCKET.get()).block(() -> (LiquidBlock)CreateMfModBlocks.NETHERALAVS.get());
+public abstract class NetheralavsFluid extends BaseFlowingFluid {
+    public static final BaseFlowingFluid.Properties PROPERTIES = new BaseFlowingFluid.Properties(
+            CreateMfModFluidTypes.NETHERALAVS_TYPE,
+            CreateMfModFluids.NETHERALAVS,
+            CreateMfModFluids.FLOWING_NETHERALAVS
+    );
 
     private NetheralavsFluid() {
         super(PROPERTIES);
     }
 
-    public ParticleOptions getDripParticle() {
-        return ParticleTypes.LARGE_SMOKE;
-    }
-
-    public static class Flowing
-    extends NetheralavsFluid {
-        protected void createFluidStateDefinition(StateDefinition.Builder<Fluid, FluidState> builder) {
-            super.createFluidStateDefinition(builder);
-            builder.add(new Property[]{LEVEL});
-        }
-
-        public int getAmount(FluidState state) {
-            return (Integer)state.getValue((Property)LEVEL);
-        }
-
-        public boolean isSource(FluidState state) {
-            return false;
-        }
-    }
-
-    public static class Source
-    extends NetheralavsFluid {
+    public static class Source extends NetheralavsFluid {
+        @Override
         public int getAmount(FluidState state) {
             return 8;
         }
 
+        @Override
         public boolean isSource(FluidState state) {
             return true;
         }
     }
-}
 
+    public static class Flowing extends NetheralavsFluid {
+        @Override
+        protected void createFluidStateDefinition(net.minecraft.world.level.block.state.StateDefinition.Builder<Fluid, FluidState> builder) {
+            super.createFluidStateDefinition(builder);
+            builder.add(LEVEL);
+        }
+
+        @Override
+        public int getAmount(FluidState state) {
+            return state.getValue(BlockStateProperties.LEVEL_FLOWING);
+        }
+
+        @Override
+        public boolean isSource(FluidState state) {
+            return false;
+        }
+    }
+}
