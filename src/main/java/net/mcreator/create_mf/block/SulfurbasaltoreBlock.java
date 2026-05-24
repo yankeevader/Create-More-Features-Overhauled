a052a34f4e0c7517ac52870b1e97b93c129d624f
Yankeevader <36558243+yankeevader@users.noreply.github.com>
2026-05-24 16:57:05 -0700

    Clean sulfur basalt ore block state generics
    - Removed raw Property and Comparable casts from sulfur basalt ore state handling
    - Replaced deprecated BlockState rotate usage in mirror handling
    - Cleaned generated block state code without changing behavior

--- src/main/java/net/mcreator/create_mf/block/SulfurbasaltoreBlock.java
+++ src/main/java/net/mcreator/create_mf/block/SulfurbasaltoreBlock.java
@@ -2,10 +2,12 @@ package net.mcreator.create_mf.block;
 
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
+import net.minecraft.util.RandomSource;
 import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.DirectionalBlock;
+import net.minecraft.world.level.block.HorizontalDirectionalBlock;
 import net.minecraft.world.level.block.Mirror;
 import net.minecraft.world.level.block.Rotation;
 import net.minecraft.world.level.block.SoundType;
@@ -13,37 +15,50 @@ import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.properties.DirectionProperty;
-import net.minecraft.world.level.block.state.properties.NoteBlockInstrument;
-import net.minecraft.world.level.block.state.properties.Property;
+import net.minecraft.world.level.material.MapColor;
 
-public class SulfurbasaltoreBlock
-extends Block {
-    public static final DirectionProperty FACING = DirectionalBlock.FACING;
+public class SulfurbasaltoreBlock extends Block {
+    public static final DirectionProperty FACING = HorizontalDirectionalBlock.FACING;
 
     public SulfurbasaltoreBlock() {
-        super(BlockBehaviour.Properties.of().instrument(NoteBlockInstrument.BASEDRUM).sound(SoundType.BASALT).strength(7.0f, 10.0f).requiresCorrectToolForDrops());
-        this.registerDefaultState((BlockState)((BlockState)this.stateDefinition.any()).setValue((Property)FACING, (Comparable)Direction.NORTH));
+        super(BlockBehaviour.Properties.of()
+                .mapColor(MapColor.COLOR_BLACK)
+                .sound(SoundType.BASALT)
+                .strength(1.0f, 10.0f)
+                .requiresCorrectToolForDrops()
+                .lightLevel(state -> 5));
+
+        this.registerDefaultState(this.stateDefinition.any().setValue(FACING, Direction.NORTH));
     }
 
+    @Override
     public int getLightBlock(BlockState state, BlockGetter worldIn, BlockPos pos) {
         return 15;
     }
 
+    @Override
     protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
         super.createBlockStateDefinition(builder);
-        builder.add(new Property[]{FACING});
+        builder.add(FACING);
     }
 
+    @Override
     public BlockState getStateForPlacement(BlockPlaceContext context) {
-        return (BlockState)super.getStateForPlacement(context).setValue((Property)FACING, (Comparable)context.getNearestLookingDirection().getOpposite());
+        return super.getStateForPlacement(context).setValue(FACING, context.getHorizontalDirection().getOpposite());
     }
 
-    public BlockState rotate(BlockState state, Rotation rot) {
-        return (BlockState)state.setValue((Property)FACING, (Comparable)rot.rotate((Direction)state.getValue((Property)FACING)));
+    @Override
+    public BlockState rotate(BlockState state, Rotation rotation) {
+        return state.setValue(FACING, rotation.rotate(state.getValue(FACING)));
     }
 
-    public BlockState mirror(BlockState state, Mirror mirrorIn) {
-        return state.rotate(mirrorIn.getRotation((Direction)state.getValue((Property)FACING)));
+    @Override
+    public BlockState mirror(BlockState state, Mirror mirror) {
+        return rotate(state, mirror.getRotation(state.getValue(FACING)));
+    }
+
+    @Override
+    public void animateTick(BlockState state, Level world, BlockPos pos, RandomSource random) {
+        super.animateTick(state, world, pos, random);
     }
 }
-
