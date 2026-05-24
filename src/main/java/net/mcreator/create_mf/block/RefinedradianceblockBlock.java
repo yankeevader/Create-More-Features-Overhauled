f204b8011691df880f2383035ffc68a2eb4a02e2
Yankeevader <36558243+yankeevader@users.noreply.github.com>
2026-05-24 16:55:25 -0700

    Clean refined radiance block state generics
    - Removed raw Property and Comparable casts from refined radiance block state handling
    - Replaced deprecated BlockState rotate usage in mirror handling
    - Added explicit overrides and cleaned formatting without changing behavior

--- src/main/java/net/mcreator/create_mf/block/RefinedradianceblockBlock.java
+++ src/main/java/net/mcreator/create_mf/block/RefinedradianceblockBlock.java
@@ -15,41 +15,53 @@ import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.properties.DirectionProperty;
 import net.minecraft.world.level.block.state.properties.NoteBlockInstrument;
-import net.minecraft.world.level.block.state.properties.Property;
 import net.minecraft.world.level.material.MapColor;
 
-public class RefinedradianceblockBlock
-extends Block {
+public class RefinedradianceblockBlock extends Block {
     public static final DirectionProperty FACING = HorizontalDirectionalBlock.FACING;
 
     public RefinedradianceblockBlock() {
-        super(BlockBehaviour.Properties.of().instrument(NoteBlockInstrument.BASEDRUM).mapColor(MapColor.QUARTZ).sound(SoundType.NETHER_BRICKS).strength(7.0f, 50.0f).lightLevel(s -> 15).requiresCorrectToolForDrops().hasPostProcess((bs, br, bp) -> true).emissiveRendering((bs, br, bp) -> true));
-        this.registerDefaultState((BlockState)((BlockState)this.stateDefinition.any()).setValue((Property)FACING, (Comparable)Direction.NORTH));
+        super(BlockBehaviour.Properties.of()
+                .instrument(NoteBlockInstrument.BASEDRUM)
+                .mapColor(MapColor.QUARTZ)
+                .sound(SoundType.NETHER_BRICKS)
+                .strength(7.0f, 50.0f)
+                .lightLevel(state -> 15)
+                .requiresCorrectToolForDrops()
+                .hasPostProcess((state, getter, pos) -> true)
+                .emissiveRendering((state, getter, pos) -> true));
+
+        this.registerDefaultState(this.stateDefinition.any().setValue(FACING, Direction.NORTH));
     }
 
+    @Override
     public int getLightBlock(BlockState state, BlockGetter worldIn, BlockPos pos) {
         return 15;
     }
 
+    @Override
     protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
         super.createBlockStateDefinition(builder);
-        builder.add(new Property[]{FACING});
+        builder.add(FACING);
     }
 
+    @Override
     public BlockState getStateForPlacement(BlockPlaceContext context) {
-        return (BlockState)super.getStateForPlacement(context).setValue((Property)FACING, (Comparable)context.getHorizontalDirection().getOpposite());
+        return super.getStateForPlacement(context).setValue(FACING, context.getHorizontalDirection().getOpposite());
     }
 
-    public BlockState rotate(BlockState state, Rotation rot) {
-        return (BlockState)state.setValue((Property)FACING, (Comparable)rot.rotate((Direction)state.getValue((Property)FACING)));
+    @Override
+    public BlockState rotate(BlockState state, Rotation rotation) {
+        return state.setValue(FACING, rotation.rotate(state.getValue(FACING)));
     }
 
-    public BlockState mirror(BlockState state, Mirror mirrorIn) {
-        return state.rotate(mirrorIn.getRotation((Direction)state.getValue((Property)FACING)));
+    @Override
+    public BlockState mirror(BlockState state, Mirror mirror) {
+        return rotate(state, mirror.getRotation(state.getValue(FACING)));
     }
 
+    @Override
     public float getEnchantPowerBonus(BlockState state, LevelReader world, BlockPos pos) {
         return 1.0f;
     }
 }
-
