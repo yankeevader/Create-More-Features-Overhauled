12404d03d0bd3578af89bbfffe1033b9236b7bf4
Yankeevader <36558243+yankeevader@users.noreply.github.com>
2026-05-24 16:56:15 -0700

    Clean refined radiance casing block state generics
    - Removed raw Property and Comparable casts from refined radiance casing state handling
    - Replaced deprecated BlockState rotate usage in mirror handling
    - Added explicit overrides and cleaned formatting without changing behavior

--- src/main/java/net/mcreator/create_mf/block/RefinedradiancecasingBlock.java
+++ src/main/java/net/mcreator/create_mf/block/RefinedradiancecasingBlock.java
@@ -13,36 +13,42 @@ import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.properties.DirectionProperty;
-import net.minecraft.world.level.block.state.properties.Property;
 
-public class RefinedradiancecasingBlock
-extends Block {
+public class RefinedradiancecasingBlock extends Block {
     public static final DirectionProperty FACING = HorizontalDirectionalBlock.FACING;
 
     public RefinedradiancecasingBlock() {
-        super(BlockBehaviour.Properties.of().sound(SoundType.ANCIENT_DEBRIS).strength(7.0f, 10.0f).requiresCorrectToolForDrops());
-        this.registerDefaultState((BlockState)((BlockState)this.stateDefinition.any()).setValue((Property)FACING, (Comparable)Direction.NORTH));
+        super(BlockBehaviour.Properties.of()
+                .sound(SoundType.ANCIENT_DEBRIS)
+                .strength(7.0f, 10.0f)
+                .requiresCorrectToolForDrops());
+
+        this.registerDefaultState(this.stateDefinition.any().setValue(FACING, Direction.NORTH));
     }
 
+    @Override
     public int getLightBlock(BlockState state, BlockGetter worldIn, BlockPos pos) {
         return 15;
     }
 
+    @Override
     protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
         super.createBlockStateDefinition(builder);
-        builder.add(new Property[]{FACING});
+        builder.add(FACING);
     }
 
+    @Override
     public BlockState getStateForPlacement(BlockPlaceContext context) {
-        return (BlockState)super.getStateForPlacement(context).setValue((Property)FACING, (Comparable)context.getHorizontalDirection().getOpposite());
+        return super.getStateForPlacement(context).setValue(FACING, context.getHorizontalDirection().getOpposite());
     }
 
-    public BlockState rotate(BlockState state, Rotation rot) {
-        return (BlockState)state.setValue((Property)FACING, (Comparable)rot.rotate((Direction)state.getValue((Property)FACING)));
+    @Override
+    public BlockState rotate(BlockState state, Rotation rotation) {
+        return state.setValue(FACING, rotation.rotate(state.getValue(FACING)));
     }
 
-    public BlockState mirror(BlockState state, Mirror mirrorIn) {
-        return state.rotate(mirrorIn.getRotation((Direction)state.getValue((Property)FACING)));
+    @Override
+    public BlockState mirror(BlockState state, Mirror mirror) {
+        return rotate(state, mirror.getRotation(state.getValue(FACING)));
     }
 }
-
