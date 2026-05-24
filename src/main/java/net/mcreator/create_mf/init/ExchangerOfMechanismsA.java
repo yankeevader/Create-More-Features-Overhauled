1eb0eed585f7ab195b1bc16660294aa80fc12c54
Yankeevader <36558243+yankeevader@users.noreply.github.com>
2026-05-23 19:38:52 -0700

    Rebalance mechanism exchanger trades
    Smooths Mechanism Exchanger trade-up costs while preserving its bartering identity. A few higher-value exchanges now require slightly more emerald support, keeping vanilla-style fixed trade bases and normal villager reroll compatibility.

--- src/main/java/net/mcreator/create_mf/init/ExchangerOfMechanismsA.java
+++ src/main/java/net/mcreator/create_mf/init/ExchangerOfMechanismsA.java
@@ -23,11 +23,11 @@ public class ExchangerOfMechanismsA {
         CmfTradeGuard.safeAdd((List) event.getTrades().get(1), new BasicItemListing(ExchangerOfMechanismsA.createItem("cogwheel", 2), ExchangerOfMechanismsA.createItem("large_cogwheel", 1), 15, 3, 0.01f));
         CmfTradeGuard.safeAdd((List) event.getTrades().get(1), new BasicItemListing(ExchangerOfMechanismsA.createItem("clutch", 1), ExchangerOfMechanismsA.createItem("gearshift", 1), 20, 3, 0.01f));
 
-        CmfTradeGuard.safeAdd((List) event.getTrades().get(2), new BasicItemListing(ExchangerOfMechanismsA.createItem("water_wheel", 1), new ItemStack((ItemLike) Items.EMERALD, 2), ExchangerOfMechanismsA.createItem("large_water_wheel", 1), 10, 5, 0.02f));
+        CmfTradeGuard.safeAdd((List) event.getTrades().get(2), new BasicItemListing(ExchangerOfMechanismsA.createItem("water_wheel", 1), new ItemStack((ItemLike) Items.EMERALD, 3), ExchangerOfMechanismsA.createItem("large_water_wheel", 1), 10, 5, 0.02f));
         CmfTradeGuard.safeAdd((List) event.getTrades().get(2), new BasicItemListing(ExchangerOfMechanismsA.createItem("whisk", 1), ExchangerOfMechanismsA.createItem("propeller", 1), 10, 5, 0.02f));
         CmfTradeGuard.safeAdd((List) event.getTrades().get(2), new BasicItemListing(ExchangerOfMechanismsA.createItem("wooden_bracket", 6), ExchangerOfMechanismsA.createItem("metal_bracket", 1), 15, 5, 0.02f));
 
-        CmfTradeGuard.safeAdd((List) event.getTrades().get(3), new BasicItemListing(ExchangerOfMechanismsA.createItem("depot", 1), new ItemStack((ItemLike) Items.EMERALD, 2), ExchangerOfMechanismsA.createItem("weighted_ejector", 1), 10, 7, 0.02f));
+        CmfTradeGuard.safeAdd((List) event.getTrades().get(3), new BasicItemListing(ExchangerOfMechanismsA.createItem("depot", 1), new ItemStack((ItemLike) Items.EMERALD, 3), ExchangerOfMechanismsA.createItem("weighted_ejector", 1), 10, 7, 0.02f));
         CmfTradeGuard.safeAdd((List) event.getTrades().get(3), new BasicItemListing(ExchangerOfMechanismsA.createItem("shaft", 16), ExchangerOfMechanismsA.createItem("andesite_funnel", 1), 18, 7, 0.02f));
         CmfTradeGuard.safeAdd((List) event.getTrades().get(3), new BasicItemListing(ExchangerOfMechanismsA.createItem("shaft", 28), ExchangerOfMechanismsA.createItem("andesite_tunnel", 1), 18, 7, 0.02f));
 
@@ -35,7 +35,7 @@ public class ExchangerOfMechanismsA {
         CmfTradeGuard.safeAdd((List) event.getTrades().get(4), new BasicItemListing(ExchangerOfMechanismsA.createItem("andesite_alloy", 24), ExchangerOfMechanismsA.createItem("placard", 1), 20, 8, 0.03f));
         CmfTradeGuard.safeAdd((List) event.getTrades().get(4), new BasicItemListing(ExchangerOfMechanismsA.createItem("encased_chain_drive", 8), ExchangerOfMechanismsA.createItem("adjustable_chain_gearshift", 1), 25, 8, 0.03f));
 
-        CmfTradeGuard.safeAdd((List) event.getTrades().get(5), new BasicItemListing(ExchangerOfMechanismsA.createItem("chest", 1), new ItemStack((ItemLike) Items.EMERALD, 3), ExchangerOfMechanismsA.createItem("item_vault", 1), 20, 8, 0.03f));
+        CmfTradeGuard.safeAdd((List) event.getTrades().get(5), new BasicItemListing(ExchangerOfMechanismsA.createItem("chest", 1), new ItemStack((ItemLike) Items.EMERALD, 4), ExchangerOfMechanismsA.createItem("item_vault", 1), 20, 8, 0.03f));
         CmfTradeGuard.safeAdd((List) event.getTrades().get(5), new BasicItemListing(ExchangerOfMechanismsA.createItem("rail", 12), ExchangerOfMechanismsA.createItem("track", 1), 20, 8, 0.03f));
         CmfTradeGuard.safeAdd((List) event.getTrades().get(5), new BasicItemListing(ExchangerOfMechanismsA.createItem("crafter_slot_cover", 4), new ItemStack((ItemLike) Items.SLIME_BALL, 3), 25, 8, 0.03f));
     }
