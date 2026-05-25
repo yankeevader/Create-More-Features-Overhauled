02e62e41104fd37112feddc51776688d34e15a6a
Yankeevader <36558243+yankeevader@users.noreply.github.com>
2026-05-24 17:06:43 -0700

    Clean trade guard generic handling
    - Removed raw List usage from trade guard helper
    - Preserved invalid trade listing safety checks
    - Cleaned reflection variable naming and method invocation
    - Removed generated-style array boilerplate

--- src/main/java/net/mcreator/create_mf/init/CmfTradeGuard.java
+++ src/main/java/net/mcreator/create_mf/init/CmfTradeGuard.java
@@ -8,52 +8,59 @@ public final class CmfTradeGuard {
     private CmfTradeGuard() {
     }
 
-    public static boolean safeAdd(List list, Object object) {
+    public static <T> boolean safeAdd(List<T> list, T object) {
         if (list == null || object == null) {
             return false;
         }
-        if (!CmfTradeGuard.isSafeListing(object)) {
+
+        if (!isSafeListing(object)) {
             System.err.println("[Create MF TradeGuard] Skipped invalid villager trade listing: " + object.getClass().getName());
             return false;
         }
+
         return list.add(object);
     }
 
     private static boolean isSafeListing(Object object) {
         try {
-            Field[] fieldArray;
             if (!"net.neoforged.neoforge.common.BasicItemListing".equals(object.getClass().getName())) {
                 return true;
             }
-            int n = 0;
-            int n2 = 0;
-            int n3 = 0;
-            for (Field field : fieldArray = object.getClass().getDeclaredFields()) {
-                if (!"net.minecraft.world.item.ItemStack".equals(field.getType().getName())) continue;
-                ++n;
-                field.setAccessible(true);
-                Object object2 = field.get(object);
-                if (object2 == null || CmfTradeGuard.isEmptyItemStack(object2)) {
-                    ++n2;
+
+            int itemStackFieldCount = 0;
+            int emptyItemStackCount = 0;
+            int nonEmptyItemStackCount = 0;
+
+            for (Field field : object.getClass().getDeclaredFields()) {
+                if (!"net.minecraft.world.item.ItemStack".equals(field.getType().getName())) {
                     continue;
                 }
-                ++n3;
+
+                itemStackFieldCount++;
+                field.setAccessible(true);
+
+                Object fieldValue = field.get(object);
+                if (fieldValue == null || isEmptyItemStack(fieldValue)) {
+                    emptyItemStackCount++;
+                } else {
+                    nonEmptyItemStackCount++;
+                }
             }
-            if (n >= 3) {
-                return n3 >= 2 && n2 <= 1;
+
+            if (itemStackFieldCount >= 3) {
+                return nonEmptyItemStackCount >= 2 && emptyItemStackCount <= 1;
             }
+
             return true;
-        }
-        catch (Throwable throwable) {
-            System.err.println("[Create MF TradeGuard] Could not inspect trade listing safely: " + String.valueOf(throwable));
+        } catch (Throwable throwable) {
+            System.err.println("[Create MF TradeGuard] Could not inspect trade listing safely: " + throwable);
             return true;
         }
     }
 
     private static boolean isEmptyItemStack(Object object) throws Exception {
-        Method method = object.getClass().getMethod("isEmpty", new Class[0]);
-        Object object2 = method.invoke(object, new Object[0]);
-        return Boolean.TRUE.equals(object2);
+        Method method = object.getClass().getMethod("isEmpty");
+        Object result = method.invoke(object);
+        return Boolean.TRUE.equals(result);
     }
 }
-
